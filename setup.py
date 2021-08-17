fc94c27ac4a3881c311a09d15ed7da01d0803dfe
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2021-08-17 18:46:25 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -9,54 +9,21 @@ from shutil import rmtree
 import pathlib
 from setuptools import setup, Extension
 import subprocess
-from setuptools.command.build_ext import build_ext as build_ext_orig
-from setuptools.command.build_ext import build_ext
-
-from setuptools import find_packages, setup, Command
-from setuptools.command.build_ext import build_ext as _build_ext
 
 
 # Package meta-data.
 NAME            = 'PyRI'
 DESCRIPTION     = 'A package to import refractive indexes from RefractiveIndex.INFO'
 URL             = 'https://github.com/paaube/PyRI'
-EMAIL           = 'pierre-alexandre.aube@polymtl.ca'
-AUTHOR          = 'Pierre-Alexandre Aubé',
+EMAIL           = 'martin.poinsinet-de-sivry@polymtl.ca, pierre-alexandre.aube@polymtl.ca'
+AUTHOR          = 'Martin Poinsinet de Sivry, Pierre-Alexandre Aubé',
 REQUIRES_PYTHON = '>3.8.0'
-VERSION         = '0.2.30'
+VERSION         = '0.0.1'
 
 # What packages are required for this module to be executed?
-REQUIRED = ['matplotlib',
-            'pandas',
-            'numpy']
-
-
-class get_numpy_include(object):
-    """Defer numpy.get_include() until after numpy is installed."""
-
-    def __str__(self):
-        import numpy
-        return numpy.get_include()
-
-
-
-EXTRAS = {}
+REQUIRED = ['matplotlib', 'numpy']
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-
-macro = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
-
-compile_args=["-std=c++14",
-              '-fopenmp',
-              '-O3',
-              '-I/usr/include/boost',
-              '-march=native']
-
-link_args=['-I/usr/include/boost',]
-
-ext_modules = []
-
 
 try:
     with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -72,43 +39,6 @@ if not VERSION:
         exec(f.read(), about)
 else:
     about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
 
 
 # Where the magic happens:
@@ -135,17 +65,13 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Operating System :: Unix',
-        'Programming Language :: C++',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering :: Physics',
         'Intended Audience :: Science/Research',
     ],
-    # $ setup.py publish support.
-    cmdclass={'upload': UploadCommand}, #'build_ext': build_ext
 )
